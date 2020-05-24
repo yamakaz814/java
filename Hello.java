1617572a7f04bf5721efb3323c8706fb1198c8f6
yamaj <62327968+yamakaz814@users.noreply.github.com>
2020-05-24 21:26:52 +0900

    変数定数

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
-public class Hello
-{
-  public static void main(String[] args) {
+public class Hello{
+  public static void main(String[] args){
     System.out.println("おはようJava!!");
+    System.out.println("javac コンパイルするコマンド");
+    System.out.println("java  classファイルを実行");
   }
 }
