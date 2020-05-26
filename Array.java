0b89e9dac769195c65cceaeb0e23cb50b9edc8b2
yamaj <62327968+yamakaz814@users.noreply.github.com>
2020-05-26 22:36:51 +0900

    配列 参照型変数

---
+++ Array.java
@@ -0,0 +1,26 @@
+public class Array {
+  public static void main(String[] args){
+    //基本型変数と参照型変数
+    // String[] arrayA = new String[3];
+    // String[] arrayB = new String[3];
+    // System.out.println(arrayA);
+    // System.out.println(arrayB);
+    // arrayB = arrayA;
+    // System.out.println(arrayA);
+    // System.out.println(arrayB);
+    
+  }
+  // public static void main(String[] args){
+  //   String[] arrayA = {"いちご","バナナ","みかん"};
+  //   String[] arrayB;
+  //   arrayB = new String[3];
+  //   System.out.println(arrayA);
+  //   System.out.println(arrayB);
+  //   arrayB = arrayA;
+  //   arrayB[1] = "Banana";
+  //   System.out.println(arrayA[1]);
+  //   System.out.println(arrayB[1]);
+  //   System.out.println(arrayA);
+  //   System.out.println(arrayB);
+  // }
+}
