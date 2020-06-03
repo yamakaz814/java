0d19eee02b0b73cc567e3e7a38e37541b1f6c905
yamaj <62327968+yamakaz814@users.noreply.github.com>
2020-06-03 23:53:45 +0900

    fin

--- Main.java
+++ Main.java
@@ -1,10 +1,5 @@
-import comment.Zenhan;
-
 public class Main {
-  public static void main(String[] args) throws Exception{
-    Zenhan.doWarusa();
-    Zenhan.doTogame();
-    comment.Kouhan.callDeae();
-    comment.Kouhan.showMondokoro();
+  public static void main(String[] args) {
+   System.out.println(args[0] + "   " + args[1]);
   }
 }
---
+++ sixt/Hero.java
@@ -0,0 +1,9 @@
+package sixt;
+
+import java.util.*;
+
+public class Hero {
+  private String name;
+  public Hero(String name){this.name = name;}
+  public String getName(){return this.name;}
+}
