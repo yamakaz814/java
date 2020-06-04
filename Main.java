900e897e4207406780a839ce2c40df0360975f6c
yamaj <62327968+yamakaz814@users.noreply.github.com>
2020-06-04 23:17:09 +0900

    演算子と判定構造

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
   public static void main(String[] args) {
-   System.out.println(args[0] + "   " + args[1]);
+    Object a = new Sample(10);
+    Object b = new Sample(10);
+    System.out.println(a.equals(b));
   }
 }
