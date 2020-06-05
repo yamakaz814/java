6d6a68e81def5536d7c8edb0616b1d94f7753adc
yamaj <62327968+yamakaz814@users.noreply.github.com>
2020-06-05 23:11:19 +0900

    黒本

--- Main.java
+++ Main.java
@@ -1,7 +1,64 @@
 public class Main {
   public static void main(String[] args) {
-    Object a = new Sample(10);
-    Object b = new Sample(10);
-    System.out.println(a.equals(b));
-  }
+    // Object a = new Sample(10);
+    // Object b = new Sample(10);
+    // System.out.println(a.equals(b));
+    // Integer ab =100;
+    // System.out.println(ab);
+    // ab =1000;
+    // System.out.println(ab);
+    
+  //   String array [][] = {{"a","b"},{"z"},{"c","d","e"}};
+  //   int total = 0;
+  //   for (String[] tmp:array){
+  //       total += tmp.length;
+  //   }
+  //   System.out.println(total);
+
+  //配列の複製
+  // int arrayA [] = {1,2,3};
+  // int arrayB [] = arrayA.clone();
+
+  // System.out.println(arrayA == arrayB);
+  // System.out.println(arrayA[0] == arrayB[0]);
+
+  // do while 下記のコードは無限ループ
+  // int a = 0;
+  // while (a < 5){
+  //   do{
+  //     a++;
+  //     System.out.println(a);
+  //   }while (true);
+  // }
+
+  //多次元配列の繰り返し処理
+  // int [][] array = {{1,2},{3,4}};
+  // for (int[] val : array){
+  //   for(int i:val){
+  //     System.out.printf(i + " ");
+  //   }
+  // }
+
+//  String[] array = {"A","B","C"};
+//   for (String str:array){
+//     System.out.println(str);
+//     str = "D";
+//     System.out.println(str);
+
+//   }
+//  int[] array = {1,2,3};
+//   for (int val:array){
+//     System.out.println(val);
+//     val = 4;
+//     System.out.println(val);
+
+//   }
+
+//   int num = 10;
+//   do{
+//     num++;
+//   }while(num++<10);
+//   System.out.println(num);
+
+   }
 }
