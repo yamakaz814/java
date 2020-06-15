eb4819fc2464c06de25944f785e88f9b59cdbcf7
yamaj <62327968+yamakaz814@users.noreply.github.com>
2020-06-15 22:35:36 +0900

    chap7-9

--- Main.java
+++ Main.java
@@ -1,21 +1,44 @@
 public class Main{
   public static void main(String[] args){
-    String hoge = "foo";//文字列fooをメモリに保持
-    String fuga = "foo";//文字列fooをメモリに保持
-    System.out.println(hoge == fuga);//同じ参照を持つためtrueが返る
-    hoge += "bar";//barを加えた新しい文字列のインスタンスが生成
-    fuga += "bar";//barを加えた新しい文字列のインスタンスが生成 上とは別
-    System.out.println(hoge == fuga);//文字列は同じでも異なるインスタンスの参照を持つためfalseが返る
+    String val2 = "A";
+    Function f = (val) -> {
+      System.out.println(val);
+        
+      };
+      f.test("B");
+    }
+  }
+  interface Function{
+    void test(String val);
+  }
 
-    // String str = "abc";
-    // String str = "a";
-    // str += "de";
 
-    // System.out.print(str);
+
+
+
+
+
+
+
+
+// public class Main{
+//   public static void main(String[] args){
+//     String hoge = "foo";//文字列fooをメモリに保持
+//     String fuga = "foo";//文字列fooをメモリに保持
+//     System.out.println(hoge == fuga);//同じ参照を持つためtrueが返る
+//     hoge += "bar";//barを加えた新しい文字列のインスタンスが生成
+//     fuga += "bar";//barを加えた新しい文字列のインスタンスが生成 上とは別
+//     System.out.println(hoge == fuga);//文字列は同じでも異なるインスタンスの参照を持つためfalseが返る
+
+//     // String str = "abc";
+//     // String str = "a";
+//     // str += "de";
+
+//     // System.out.print(str);
 
     
-  }
-}
+//   }
+// }
 //   interface Worker{
 //     void work();
 //   }
