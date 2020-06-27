920a5a3643ed8674940e63d9a4134313472668b1
yamaj <62327968+yamakaz814@users.noreply.github.com>
2020-06-27 23:19:24 +0900

    200626

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main{
   public static void main(String[] args){
-    String val2 = "A";
+    // String val2 = "A";
     Function f = (val) -> {
       System.out.println(val);
         
